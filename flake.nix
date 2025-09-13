8b5fcf4c6b8c4d8090ca04256cc81c62d05624f2
Ch4s3r <patrick.lampe.dittmer@gmail.com>
2025-09-13 22:59:35 +0200

    feat: add flake.nix and flake.lock for ArgoCD bootstrap environment with Kubernetes and Helm tools

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "ArgoCD bootstrap environment with Kubernetes and Helm tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        bootstrap = pkgs.writeShellApplication {
+          name = "bootstrap";
+          text = ''
+            kubectl kustomize --enable-helm apps/argocd | kubectl apply -f -
+            kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
+            kubectl apply -f apps/argocd/applicationset.yaml
+            kubectl wait --for=jsonpath='{.metadata.name}'=all-apps applicationset/all-apps -n argocd
+          '';
+        };
+
+        template = pkgs.writeShellApplication {
+          name = "template";
+          runtimeInputs = with pkgs; [ helmfile ];
+          text = ''
+            helmfile template
+          '';
+        };
+      in
+      {
+        packages = {
+          inherit bootstrap-helmfile bootstrap template;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            uutils-coreutils-noprefix
+            helmfile
+            git
+            curl
+            kubectl
+            kubernetes-helm
+            kubernetes-helmPlugins.helm-diff
+            kubernetes-helmPlugins.helm-secrets
+            kubernetes-helmPlugins.helm-s3
+            kubernetes-helmPlugins.helm-git
+            bootstrap
+            template
+          ];
+          shellHook = ''
+            echo "Bootstrap scripts available:"
+            echo "  bootstrap-helmfile - Bootstrap ArgoCD with helmfile ApplicationSet"
+            echo "  bootstrap - Bootstrap ArgoCD with ApplicationSet"
+            echo "  template - Run helmfile template"
+          '';
+        };
+      }
+    );
+}
