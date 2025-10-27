9f5c4ca74c541ac1ef647f95353a9b879c751523
Ch4s3r <patrick.lampe.dittmer@gmail.com>
2025-10-27 09:40:22 +0100

    fix: remove unused kustomization, namespace, and values files for hello-world app

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           text = ''
             kubectl kustomize --enable-helm apps/argocd | kubectl apply -f -
             kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
-            kubectl apply -f apps/argocd/applicationset.yaml
+            kubectl apply -f apps/argocd/helmfile-applicationset.yaml
             kubectl wait --for=jsonpath='{.metadata.name}'=all-apps applicationset/all-apps -n argocd
           '';
         };
